90403d1dec1a696261abe9e6710bec89d3b21cba
Bruno Costa <bruno.costa@superlogica.com>
2021-01-18 16:29:06 -0300

    retirando mensagem de usuario

--- Resources/views/user/index.blade.php
+++ Resources/views/user/index.blade.php
@@ -4,12 +4,6 @@
     <div class="card">
         <div class="card-header">{{__('Relatório de usuário')}}</div>
         <div class="card-body">
-            @if (Session::has('success') && Session::get("success"))
-                <div class="alert-flash alert alert-success mt-3 text-center" role="alert">
-                    {!! Session::get("success") !!}
-                </div>
-            @endif
-
             {{$results}}
         </div>
     </div>
