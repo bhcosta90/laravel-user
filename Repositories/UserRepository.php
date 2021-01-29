4f1a6d32f8950df3d3a5f259d44e2b54fce91c9a
Bruno Henrique da Costa <bhcosta90@gmail.com>
2021-01-29 15:42:48 -0300

    Update UserRepository.php

--- Repositories/UserRepository.php
+++ Repositories/UserRepository.php
@@ -4,6 +4,7 @@ namespace Costa\User\Repositories;
 
 use Costa\User\Entities\User;
 use JasonGuru\LaravelMakeRepository\Repository\BaseRepository;
+use Spatie\Permission\Traits\HasRoles;
 
 /**
  * Class UserRepository.
@@ -17,5 +18,16 @@ class UserRepository extends BaseRepository implements Contracts\UserContract
     public function model()
     {
         return config('auth.providers.users.model');
+    }
+
+    public function verifySpatiePermission($data){
+        $obj = $this->makeModel();
+
+        if(
+            (count($data['permissions'] ?? []) > 0 || $data['roles'] ?? []) &&
+            in_array(HasRoles::class, class_uses($obj)) == false
+        ){
+            throw new \Exception(get_class($obj) . ' do not implemented ' . HasRoles::class);
+        }
     }
 }
