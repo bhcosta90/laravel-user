c605cdc9ba70abe92116edbf6be38bec4f7736ad
Bruno Costa <bruno.costa@dafiti.com.br>
2023-05-27 16:21:31 -0300

    finalizado

--- src/Resources/views/components/permission.blade.php
+++ src/Resources/views/components/permission.blade.php
@@ -7,21 +7,21 @@
     <input type="hidden" name="{{$name}}name_model" value="{{base64_encode($model)}}" />
     @if(empty($fieldset))
     <x-card>
-        <x-card-header :title="$groupPermission['title']" :register="$save" type-register="submit" text-register="Vincular permissões" />
+        <x-card-header :title="$groupPermission['title']" :register="$save" type-register="submit" modal-close="0" text-register="Vincular permissões" />
         <x-card-body>
             <div class='row'>
                 @foreach($groupPermission['permissions'] as $id => $permission)
                     <input type="hidden" name="{{$name}}group" value="{{$groupPermission['title']}}" />
                     <div class='pull-left float-left ml-3'>
                         <div class="icheck-primary" title="{{ $permission }}">
-                            <label for="{{sha1($name.$id)}}">
+                            <label>
                                 <input
                                     @if(in_array($id, $myPermissions))
                                         checked
                                     @endif
                                     type="checkbox"
                                     value="{{$id}}"
-                                    name="{{$name}}permission[{{$groupPermission['title']}}][{{$id}}]" id="{{sha1($name.$id)}}"
+                                    name="{{$name}}permission[{{$groupPermission['title']}}][{{$id}}]"
                                 />
 
                                 {{ __($permission) }}
@@ -39,14 +39,14 @@
             <input type="hidden" name="{{$name}}group" value="{{$groupPermission['title']}}" />
             <div class='pull-left float-left mr-3'>
                 <div class="icheck-primary" title="{{ $permission }}">
-                    <label for="{{sha1($name.$id)}}">
+                    <label>
                         <input
                             @if(in_array($id, $myPermissions))
                                 checked
                             @endif
                             type="checkbox"
                             value="{{$id}}"
-                            name="{{$name}}permission[{{$groupPermission['title']}}][{{$id}}]" id="{{sha1($name.$id)}}"
+                            name="{{$name}}permission[{{$groupPermission['title']}}][{{$id}}]"
                         />
                         {{ __($permission) }}
                     </label>
