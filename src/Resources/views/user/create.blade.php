122fc4d6de1cd1fb7cdf13b15c8f4bc618980f2e
Bruno Costa <bruno.costa@dafiti.com.br>
2023-05-27 07:42:20 -0300

    finalizado

--- src/Resources/views/user/create.blade.php
+++ src/Resources/views/user/create.blade.php
@@ -3,6 +3,12 @@
 @section('content')
     <x-card>
         <x-card-header title="{{config('bhcosta90-user.user.view.create')}}" />
-        <x-card-body>{!! form($form) !!}</x-card-body>
+        <x-card-body>
+            {!! form_start($form) !!}
+            {!! form_row($form->name) !!}
+            {!! form_row($form->email) !!}
+            {!! form_row($form->password) !!}
+            {!! form_end($form) !!}
+        </x-card-body>
     </x-card>
 @endsection
